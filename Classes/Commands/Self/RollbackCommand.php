912753bf457a44a185e885524444b01b0c84bc3b
Susanne Moog <susanne.moog@typo3.com>
2016-08-16 10:39:43 +0200

    [FEATURE] Add self-update and rollback capabilities

---
+++ Classes/Commands/Self/RollbackCommand.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types = 1);
+namespace T3G\Elasticorn\Commands\Self;
+
+use Humbug\SelfUpdate\Updater;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class RollbackCommand extends Command
+{
+    /**
+     * Configure the rollback command
+     *
+     * @return void
+     */
+    protected function configure()
+    {
+        parent::configure();
+        $this
+            ->setName('self:rollback')
+            ->setDescription('Rollback elasticorn to the previous version.');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $updater = new Updater();
+        $result = $updater->rollback();
+        if (false === $result) {
+            $output->writeln('Something went wrong, rollback failed.');
+        }
+    }
+}
